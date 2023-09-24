d9f52a7f4d7beec4cb1dc2fc46cfc806bb7a77f4
VLADok228336 <97050152+VLADok228336@users.noreply.github.com>
2023-09-24 16:37:40 +0300

    Закончил 3-ю задачку

--- HW24.09/task3.py
+++ HW24.09/task3.py
@@ -1,5 +1,7 @@
-print("How much time is left to lunch?")
+print("How much time is left to lunch?", end=" ")
 lunch = int(input())
-print("How many details do you must to do?")
+print("How many details do you must to do?", end="")
 howmany = int(input())
+print(f"Before lunch you can do {lunch//4} details")
+print(f"To finish plan you should work {howmany*4//60} h {howmany*4%60} m")
 
