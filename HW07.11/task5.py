b0133a06e57f3b95e1bb2e5deff7c0f510195e4a
VLADok228336 <97050152+VLADok228336@users.noreply.github.com>
2023-11-14 15:56:38 +0300

    Update task5.py

--- HW07.11/task5.py
+++ HW07.11/task5.py
@@ -6,8 +6,7 @@ for i in range(n):
     f = input().split()
     item.append(f[0])
     price.append(f[1])
-    d[i] = {f[0]:f[1]}
-print(d)
-g= price.sort()
-k = item.sort()
+item_item = item.copy
+price_price = price.sort()
+print(price_price)
 i = 0
