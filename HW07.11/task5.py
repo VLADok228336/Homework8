d8108ca6c7e78ae83597136b0d93bd3ea07a36b6
VLADok228336 <97050152+VLADok228336@users.noreply.github.com>
2023-11-14 17:35:32 +0300

    Добавил README.txt и сделал 1-ю задачу

--- HW07.11/task5.py
+++ HW07.11/task5.py
@@ -6,7 +6,9 @@ for i in range(n):
     f = input().split()
     item.append(f[0])
     price.append(f[1])
-item_item = item.copy
-price_price = price.sort()
+item_item = item.copy()
+price_price = price.copy()
+price_price.sort()
+
 print(price_price)
 i = 0
---
+++ HW23.11/HiLo(task1).py
@@ -0,0 +1,52 @@
+from random import*
+from time import*
+def coolPrint(s1):
+    s11 = list(s1)
+    for i in s11:
+        print(i, end="")
+        sleep(0.05)
+    sleep(1)
+    print("")
+def start():
+    hello()
+def hello():
+    coolPrint("Hello boys and girls, my name is Fat Lip. And this is my friend Robert the Robot('What 'do?')")
+    coolPrint("Today we gonna teach you some fun facts about binary search, and a brand new game")
+    instructions()
+def instructions():
+    coolPrint("Let me introduce to you a brand new game/ I know you gonna love it if you give it one chance")
+    coolPrint("It's not complicated, it's not too hard. You don't even have to be a hip hop star")
+    coolPrint("That was joke")
+    coolPrint("...")
+    coolPrint("Okay, there's instructions. You gonna guess number, which my friend Robert has guessed")
+    coolPrint("Go ahead")
+    guess()
+def check(num, guess):
+    if guess == num:
+        return True
+    else:
+        return False
+def guess():
+    tries = 1
+    number = randint(0, 10000)
+    while True:
+        coolPrint("Enter a number:")
+        guess = int(input())
+        if check(number, guess):
+            coolPrint("OMG! What a nice guess!")
+            coolPrint(f"You win only with {tries} tries!")
+            coolPrint("Wanna play again(y/n)?")
+            s = input()
+            if s == "n":
+                coolPrint("Goodbye! Thanks for playing!")
+                exit("Don't forget listen to Metallica!")
+            else:
+                tries = 1
+                number = randint(0, 10000)
+        else:
+            coolPrint("That was nice try, but no")
+            if number>guess:
+                coolPrint("Higher!")
+            if number< guess:
+                coolPrint("Lower!")
+start()                
